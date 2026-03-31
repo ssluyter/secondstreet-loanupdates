431ab03a8b173293fda96e7ce41a8baa2960270b
ssluyter <steve@mysecondstreet.com>
2026-03-30 19:10:24 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -44,6 +44,7 @@ function computeRingColor(vars, milestones, closeOfEscrow) {
   var closeDate; try { closeDate = new Date(closeDateStr); closeDate.setHours(0,0,0,0); } catch(e) { return 'green'; }
   if (isNaN(closeDate.getTime())) return 'green';
   var daysUntilClose = Math.ceil((closeDate - today) / 86400000);
+  (app.status&&typeof app.status==='object'?app.status.name:app.statusName||app.status)||''
   var criticalLabels = ['PSA received','Escrow opened','Appraisal ordered','Due diligence ordered','Appraisal received','Due diligence cleared'];
   var criticalDone = milestones.filter(function(m) { return criticalLabels.indexOf(m.label) !== -1 && m.status === 'done'; }).length;
   var criticalRemaining = 6 - criticalDone;
